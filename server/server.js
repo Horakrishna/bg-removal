855af253b022838e2594fb712485f92a0460c045
Horakrishna <horakrishnaroy@gmail.com>
2024-11-10 00:01:38 +0600

    vercel added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongoDb.js'
+import userRouter from './routes/userRoutes.js'
 
 //App Config
 
@@ -15,5 +16,6 @@ app.use(cors())
 
 //Api Route
 app.get('/',(req,res)=>res.send("Api is working"))
+app.use("/api/user",userRouter)
 
 app.listen(port, ()=>console.log("server Runnig on Port" + port))
